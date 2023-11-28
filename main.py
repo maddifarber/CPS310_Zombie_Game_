b263f8f5c2363d0390dd58d250d9f4012df2a95e
maddifarber <145685943+maddifarber@users.noreply.github.com>
2023-11-27 21:56:29 -0500

    Update main.py
    I got some results that are not 0.. not acing right though.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import random
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 # Define agent states
 class AgentState:
     ALIVE = "Alive"
     INFECTED = "Infected"
     DEAD = "Dead"
+
 
 # Define agent types
 class AgentType:
@@ -14,6 +16,7 @@ class AgentType:
     ZOMBIE = "Zombie"
     DOCTOR = "Doctor"
     DEAD = "Dead"
+
 
 # Define the Agent class with inheritance
 class Agent:
@@ -31,29 +34,42 @@ class Agent:
         return f"{self.agent_type} (ID: {self.id}, Energy: {self.energy}, State: {self.state})"
 
     def update(self):
-        pass
+        # Check if the agent is dead based on energy level
+        if self.energy <= 0 and self.state != AgentState.DEAD:
+            print(f"Agent {self.id} is dead. Energy: {self.energy}")
+            self.state = AgentState.DEAD
+        else:
+            print(f"Agent {self.id} is alive. Energy: {self.energy}, State: {self.state}")
+
 
 # Define subclasses for each agent type
 class Human(Agent):
-    def __init__(self, energy_range=(0, 100)):
+    def __init__(self, energy_range=(90, 100)):
         super().__init__(AgentType.HUMAN, energy_range)
 
     def update(self):
-        self.energy -= 5
+        self.energy -= 2
+        if self.energy <= 0 and self.state != AgentState.DEAD:
+            self.state = AgentState.DEAD
+
 
 class Zombie(Agent):
     def __init__(self, energy_range=(0, 100)):
         super().__init__(AgentType.ZOMBIE, energy_range)
 
     def update(self):
-        if self.state == AgentState.ALIVE:
-            for neighbor in self.neighbors:
-                if neighbor.agent_type == AgentType.HUMAN and neighbor.state != AgentState.INFECTED:
-                    if random.random() < 0.3:
-                        neighbor.state = AgentState.INFECTED
-                        neighbor.energy -= 20
-                        self.energy += 10
-                        break
+        for neighbor in self.neighbors:
+            if neighbor.agent_type == AgentType.HUMAN and neighbor.state == AgentState.ALIVE:
+                if random.random() < 0.3:
+                    neighbor.state = AgentState.INFECTED
+                    neighbor.energy -= 20
+                    self.energy += 10
+
+                    if neighbor.energy <= 0:
+                        neighbor.state = AgentState.DEAD
+                        self.state = AgentState.DEAD
+                    break
+
 
 class Doctor(Agent):
     def __init__(self, energy_range=(0, 100)):
@@ -65,8 +81,10 @@ class Doctor(Agent):
                 self.state = AgentState.ALIVE
                 self.energy -= 30
 
+
 # Create a simulation environment
 class ZombieGame:
+
     def __init__(self, num_agents, num_steps, energy_range=(0, 100)):
         self.agents = []
         self.num_agents = num_agents
@@ -83,6 +101,13 @@ class ZombieGame:
             AgentType.DEAD: []
         }
 
+        agent_counts = {
+            AgentType.HUMAN: 0,
+            AgentType.ZOMBIE: 0,
+            AgentType.DOCTOR: 0,
+            AgentType.DEAD: 0
+        }
+
         for _ in range(num_agents):
             agent_type = random.choice([AgentType.HUMAN, AgentType.ZOMBIE, AgentType.DOCTOR])
             agent = None
@@ -93,14 +118,22 @@ class ZombieGame:
             elif agent_type == AgentType.DOCTOR:
                 agent = Doctor(energy_range)
             self.agents.append(agent)
+            agent_counts[agent_type] += 1
+
+        for agent_type in agent_counts:
+            self.population_data[agent_type].append(agent_counts[agent_type])
 
     def run_simulation(self):
         for step in range(self.num_steps):
+            for agent in self.agents:
+                agent.update()
+
+
+            self.agents = [agent for agent in self.agents if agent.state != AgentState.DEAD]
+
             self.agent_statuses.append([str(agent) for agent in self.agents])
 
-            # TODO
-            #   ALIVE counts for each iteration
-            agent_counts = {
+            agent_counts_before = {
                 AgentType.HUMAN: 0,
                 AgentType.ZOMBIE: 0,
                 AgentType.DOCTOR: 0,
@@ -108,25 +141,42 @@ class ZombieGame:
             }
 
             for agent in self.agents:
-                # TODO: Inside this loop, we update ALIVE counts.
-                agent.update()
-                agent.neighbors = random.sample(self.agents, min(5, self.num_agents))  # Random neighbors
-
                 if agent.state == AgentState.DEAD:
-                    agent_counts[AgentType.DEAD] += 1
-                    continue
-                if agent.state == AgentType.HUMAN:
-                    self.population_data[AgentType.HUMAN].append(1)
+                    agent_counts_before[AgentType.DEAD] += 1
+                elif agent.state == AgentType.HUMAN:
+                    agent_counts_before[AgentType.HUMAN] += 1
                 elif agent.state == AgentType.ZOMBIE:
-                    self.population_data[AgentType.ZOMBIE].append(1)
+                    agent_counts_before[AgentType.ZOMBIE] += 1
                 elif agent.state == AgentType.DOCTOR:
-                    self.population_data[AgentType.DOCTOR].append(1)
+                    agent_counts_before[AgentType.DOCTOR] += 1
 
-            # Correctly access the corresponding list based on agent_type
-            # Summarize ALIVE counts.
-            self.population_data[AgentType.HUMAN].append(agent_counts[AgentType.HUMAN])
-            self.population_data[AgentType.DOCTOR].append(agent_counts[AgentType.DOCTOR])
-            self.population_data[AgentType.ZOMBIE].append(agent_counts[AgentType.ZOMBIE])
+            for agent in self.agents:
+                agent.update()
+
+            agent_counts_after = {
+                AgentType.HUMAN: 0,
+                AgentType.ZOMBIE: 0,
+                AgentType.DOCTOR: 0,
+                AgentType.DEAD: 0
+            }
+
+            for agent in self.agents:
+                if agent.state == AgentState.DEAD:
+                    agent_counts_after[AgentType.DEAD] += 1
+                elif agent.state == AgentType.HUMAN:
+                    agent_counts_after[AgentType.HUMAN] += 1
+                elif agent.state == AgentType.ZOMBIE:
+                    agent_counts_after[AgentType.ZOMBIE] += 1
+                elif agent.state == AgentType.DOCTOR:
+                    agent_counts_after[AgentType.DOCTOR] += 1
+
+            # Summarize counts for each state after agents are updated
+            self.population_data[AgentType.HUMAN].append(agent_counts_after[AgentType.HUMAN])
+            self.population_data[AgentType.DOCTOR].append(agent_counts_after[AgentType.DOCTOR])
+            self.population_data[AgentType.ZOMBIE].append(agent_counts_after[AgentType.ZOMBIE])
+            self.population_data[AgentType.DEAD].append(agent_counts_after[AgentType.DEAD])
+
+            self.agent_statuses.append([str(agent) for agent in self.agents])
 
         self.save_simulation_data()
         self.plot_simulation_results()
@@ -149,19 +199,21 @@ class ZombieGame:
     def plot_simulation_results(self):
         plt.figure(figsize=(12, 6))
         for agent_type in AgentType.__dict__.values():
-            if agent_type in self.population_data and self.population_data[agent_type]:
-                plt.plot(range(self.num_steps), self.population_data[agent_type], label=agent_type)
+            if isinstance(agent_type, str) and agent_type in self.population_data and self.population_data[agent_type]:
+                data_length = min(len(self.population_data[agent_type]), self.num_steps)
+                plt.plot(range(data_length), self.population_data[agent_type][:data_length], label=agent_type)
         plt.xlabel("Time Step")
         plt.ylabel("Population Count")
         plt.legend()
         plt.savefig("population_plot.png")
         plt.show()
 
+        plt.ioff()
+        plt.show()
+
 
 if __name__ == "__main__":
     num_agents_param = 100
-    num_steps_param = 50
+    num_steps_param = 5
     game = ZombieGame(num_agents_param, num_steps_param)
     game.run_simulation()
-
-
